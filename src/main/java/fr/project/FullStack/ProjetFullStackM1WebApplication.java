ccd32b1900f3aa714395ae27f7470fabc89ba057
Mhilan <43454965+mLn023@users.noreply.github.com>
2020-11-18 18:34:22 +0100

    Initial commit

---
+++ src/main/java/fr/project/FullStack/ProjetFullStackM1WebApplication.java
@@ -0,0 +1,13 @@
+package fr.project.FullStack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetFullStackM1WebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetFullStackM1WebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/project/FullStack/ProjetFullStackM1WebApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.project.FullStack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetFullStackM1WebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
